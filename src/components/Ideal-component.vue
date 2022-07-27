49b956f5d8487a707515b6be5744b92a6e7670f4
stan-BK <2568143457@qq.com>
2022-07-27 17:02:02 +0800

    feat: 添加项目logo

---
+++ src/components/Ideal-component.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+const IdealComponent = 'deal-component'
+let idx = 0
+const I = ref()
+const text = ref('')
+const isLoad = ref(false)
+function setName() {
+  text.value += IdealComponent[idx++]
+  if (idx != IdealComponent.length) {
+    setTimeout(setName, idx > 4 ? 100 : 300)
+  } else {
+    isLoad.value = true
+  }
+}
+setTimeout(setName, 1000)
+
+watch(isLoad, () => {
+  setTimeout(() => {
+    I.value.classList.remove('I-sparkle')
+    I.value.classList.add('I-recover')
+    setTimeout(() => translateX(120, -38), 1000)
+  }, 1000)
+})
+
+function translateX(from: number, to: number) {
+  let a = Math.max(from, to)
+  const b = Math.min(from, to)
+  requestAnimationFrame(changeX)
+
+  function changeX() {
+    if (a >= b) {
+      a -= 2
+      I.value.setAttribute('x', a)
+      requestAnimationFrame(changeX)
+    }
+  }
+}
+</script>
+<template>=
+  <svg width="300" height="50" viewBox="0 0 100 30">
+    <text x="-30" y="25">
+      <tspan fill="white" class="text">{{ text }}</tspan>
+      <tspan fill="white" class="I I-sparkle" ref="I">I</tspan>
+    </text>
+  </svg>
+</template>
+<style>
+.I {
+  position: absolute;
+  font-size: 25px;
+  transition: font-size 3s 1s;
+}
+.I-recover {
+  font-size: 45px;
+}
+.I-sparkle {
+  animation: sparkle .8s infinite; 
+}
+@keyframes sparkle {
+  from {
+    opacity: 0;
+  }
+}
+.text { 
+  font-size: 20px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import IdealComponent from '@/components/Ideal-component.vue'
+</script>
+<template>
+  <ideal-component></ideal-component>
+</template>
+<style>
+</style>
