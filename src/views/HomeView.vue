4742296802119e4a8bc541c7a56bff3067dfda61
stan-BK <2568143457@qq.com>
2022-10-02 21:20:09 +0800

    update: 实现页面整体布局样式

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,42 @@
 import IdealComponent from '@/components/Ideal-component.vue'
 </script>
 <template>
-  <header>
-    <div style="float: left;">
-      <ideal-component></ideal-component>
-    </div>
-    <div>asdofihaf</div>
+  <header class="theme">
+    <ideal-component></ideal-component>
   </header>
-  <main>
+  <nav class="theme">
 
+  </nav>
+  <main class="theme">
+    
   </main>
-  <footer>
-
-  </footer>
 </template>
-<style>
+<style scoped>
+header {
+  position: fixed;
+  height: 80px;
+  border-top: none;
+  border-left: none;
+  border-bottom-right-radius: 20px;
+  padding: 10px 20px 20px 35px;
+}
+nav {
+  position: fixed;
+  bottom: 0;
+  width: 250px;
+  height: calc(100vh - 100px);
+  border-bottom: none;
+  border-left: none;
+  border-top-right-radius: 20px;
+}
+main {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  width: calc(100vw - 380px);
+  height: calc(100vh - 40px);
+  border-top-left-radius: 20px;
+  border-bottom: none;
+  border-right: none;
+}
 </style>
