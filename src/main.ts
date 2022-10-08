0310fed2d7c9f2ad7d94de81730542785b13a753
stan-BK <2568143457@qq.com>
2022-10-08 09:54:33 +0800

    update: 完善滚动条功能逻辑

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import EasierScroll from 'easier-scroll-vue'
-
 import './assets/main.css'
 
 const app = createApp(App)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
 import IdealComponent from '@/components/Ideal-component.vue'
+
+const scrollbar = ref()
+const bar = ref()
 const pY = ref(0)
 function updatePY(y: number) {
   pY.value = y
 }
+
+onMounted(() => {
+  const sb: HTMLElement = scrollbar.value
+  const height = sb.getBoundingClientRect().height
+  sb.addEventListener('mousedown', (e) => {
+    sb.addEventListener('mousemove', mouseMove)
+    document.addEventListener('mouseup', mouseUp)
+  })
+
+  function mouseMove(e) {
+    pY.value = (e.offsetY + 5) / height
+  }
+
+  function mouseUp(e) {
+    sb.removeEventListener('mousemove', mouseMove)
+    document.removeEventListener('mouseup', mouseUp)
+  }
+})
 </script>
 <template>
   <header class="theme">
@@ -12,8 +33,9 @@ function updatePY(y: number) {
   <nav class="theme">
     
   </nav>
-  <div class="scrollbar theme">
-    <div class="bar theme" :style="{ top: `calc(${ pY * 100 }% - 50px)`}"></div>
+  <div class="scrollbar-shape theme"></div>
+  <div class="scrollbar" ref="scrollbar">
+    <div class="bar theme" :style="{ top: `calc(${ pY * 100 }% - 50px)`}" ref="bar"></div>
   </div>
   <easier-scroll :scrollPercentY="pY" @scrollY="updatePY">
     <main class="theme">
@@ -50,21 +72,29 @@ main {
   border-bottom: none;
   border-right: none;
 }
-.scrollbar {
+.scrollbar-shape {
   position: fixed;
-  bottom: 50px;
+  bottom: 60px;
   left: 300px;
   width: 30px;
-  height: calc(100vh - 200px);
+  height: calc(100vh - 220px);
   border-radius: 5px;
+}
+.scrollbar {
+  position: fixed;
+  bottom: 60px;
+  left: 290px;
+  width: 50px;
+  height: calc(100vh - 220px);
+  border-width: 5px;
 }
 .bar {
   position: absolute;
   top: 0;
-  left: -15px;
   width: 50px;
   height: 100px;
   border-radius: 20px;
+  pointer-events: none;
 }
 .bar::before {
   position: absolute;
@@ -76,6 +106,7 @@ main {
   height: 5px;
   background-color: #666;
   border-radius: 2px;
+  pointer-events: none;
 }
 .bar::after {
   position: absolute;
@@ -87,5 +118,6 @@ main {
   height: 5px;
   background-color: #666;
   border-radius: 2px;
+  pointer-events: none;
 }
 </style>
